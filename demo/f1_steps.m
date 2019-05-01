52eee22489a8431498395d88b420885409f17eee
Matjaž Mav <matjaz.mav@gmail.com>
2019-05-01 16:40:08 +0200

    visualized f1 steps

---
+++ demo/f1_steps.m
@@ -0,0 +1,19 @@
+I = imread('../resources/datasets/base/2t.jpg');
+
+[h,w,~] = size(I);
+
+masks = generate_color_filter_mask();
+
+mask1 = uint8(repmat(masks{1}, h/2, w/2) * 255);
+resampled1 = rgb2cfa(I, masks{1});
+reinterpolated1 = cfa_interpolation(resampled1, 'bilinear');
+
+
+
+
+figure(1); clf;
+imshow(mask1);
+figure(2); clf;
+imshow(resampled1);
+figure(3); clf;
+imshow(reinterpolated1);
